79f6dab8f5a7169371d924f93ee03c7348daeb39
Gayrat-github <164168342+Gayrat-github@users.noreply.github.com>
2024-03-20 17:46:06 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+from shape import shape
+
+print(shape.get_area(1))                    # площадь круга
+print(shape.get_area(3, 4, 5))              # площадь треугольника
+print(shape.is_right_triangle(3, 4, 5))     # является ли треугольник прямоугольным
